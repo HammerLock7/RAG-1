b895577709fc1affa1cc7b00193e18c550361db5
Shaun Nchabeleng <160620102+HammerLock7@users.noreply.github.com>
2025-05-14 16:47:08 +0200

    added new lines

--- start-3.py
+++ start-3.py
@@ -1,12 +1,17 @@
+from dotenv import load_dotenv
+import os
 import streamlit as st
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
+from langchain_community.llms import Ollama
 
+#load environment variables from the .env file
+load_dotenv()
 
 #Load documents
 docs = PyPDFLoader("Thermochemistry5.1-5.2.pdf").load()
@@ -16,13 +21,13 @@ splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=1500)
 chunks = splitter.split_documents(docs)
 
 # create a vector store
-embeddings = OpenAIEmbeddings()
-db = FAISS.from_documents(docs, embeddings)
+embeddings = OllamaEmbeddings(model="llama3.2")
+db = FAISS.from_documents(chunks, embeddings)
 
 # Set-up a retriever and QA chain
 retriever = db.as_retriever()
-llm = OpenAI(temperature=0.7)
-qa_chain = RetrievalQA.from_chain_type(llm=OpenAI(), retriever=retriever)
+llm = Ollama(model="llama3.2",temperature=0.7)
+qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 
 # streamlit UI
 st.title("RAG CHATBOT")
